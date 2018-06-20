c3e07a4fd215f6f035227f8989b3da06d6a8cfd9
oulipo <laudesj@gmail.com>
2018-06-20 14:56:05 +0200

    modif json messages

--- serveur/api/messages.php
+++ serveur/api/messages.php
@@ -7,4 +7,7 @@ $stmt = $pdo->prepare("SELECT * FROM messages WHERE room_id = ?");
 $stmt->execute([$room_id]);
 $messages = $stmt->fetchAll();
 
-echo json_encode($messages);
+echo json_encode([
+    "status" => "OK",
+    "messages" => $messages
+]);
